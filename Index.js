cb2ae8eacbafcfc7e3d5a68db3a444eaedb37d07
pxlkeshi <101736130+pxlkeshi@users.noreply.github.com>
2025-04-21 12:46:58 +0400

    Index.js

---
+++ Index.js
@@ -0,0 +1,10 @@
+let myAge=21;//change to add your own age
+let earlyYears=2;;
+earlyYears=earlyYears*10.5;
+myAge=myAge-2;
+let laterYears=myAge;
+laterYears=laterYears*4;
+let myAgeInDogYears=earlyYears+laterYears;
+myName='(YOUR NAME)';
+myName=myName.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years`);
